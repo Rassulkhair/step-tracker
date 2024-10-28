41d06ed0b82a7bccd5a9b7897f4508d9cf840c19
Rassulkhair <rassulkhairshokanov@gmail.com>
2024-10-28 22:32:49 +0500

    Реализована метод addNewNumberStepPerDay

--- src/main/java/org/example/StepTracker.java
+++ src/main/java/org/example/StepTracker.java
@@ -1,10 +1,12 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class StepTracker {
     Scanner scanner;
     MonthData[] monthData = new MonthData[12];
+    final int DAYS = 30;
 
     StepTracker(Scanner scan) {
         scanner = scan;
@@ -14,6 +16,26 @@ public class StepTracker {
     }
 
     public void addNewNumberStepsPerDay() {
+        System.out.println("Введи день");
+        int day = scanner.nextInt();
+        System.out.println("Введи месяц");
+        int month = scanner.nextInt();
+        System.out.println("Введи кол-во шагов");
+        int steps = scanner.nextInt();
+        if ((month <= monthData.length && month > 0) && (day <= DAYS && day > 0) && steps > 0) {
+            for (int i = 0; i <= monthData.length - 1; i++) {
+                if (i == month - 1) {
+                    for (int j = 0; j <= monthData[i].days.length - 1; j++) {
+                        if (j == day - 1) {
+                            monthData[i].days[j] = steps;
+                        }
+                    }
+                }
+            }
+
+        } else {
+            System.out.println("Некорректные данные");
+        }
     }
 
     public void changeStepGoal() {
